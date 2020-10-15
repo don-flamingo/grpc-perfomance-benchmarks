5e21e95c0113c7de9966fdc9545b340084af75c4
Patryk Roguszewski <patryk.roguszewski@gmail.com>
2020-10-15 21:23:42 +0200

    benchmark

--- src/Grpc.Performance.Grpc/Program.cs
+++ src/Grpc.Performance.Grpc/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace Grpc.Perfomance.Grpc
@@ -19,6 +20,16 @@ namespace Grpc.Perfomance.Grpc
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.ListenLocalhost(5000, 
+                            o => o.Protocols =
+                                HttpProtocols.Http2);
+                    });
+                    
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
---
+++ src/Grpc.Performance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Grpc.Net.Client;
+using Grpc.Performance.Contracts.Big;
+using ProtoBuf.Grpc.Client;
+
+
+namespace Grpc.Performance
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            BenchmarkRunner.Run<Grpc>();
+        }
+    }
+}
